6e9f97b709739e2af94d1a17f9248a097f16d857
Dureke Sanchez <dreamfox@berlin.cs.colostate.edu>
2024-10-20 09:29:07 -0600

    added Piece class

---
+++ Piece.py
@@ -0,0 +1,21 @@
+import Color
+
+class Piece(Color):
+    count = 0
+
+    def __init__(self, color, x, y):
+        self.color = color
+        self.x = x
+        self.y = y 
+        self.ID = self.count
+        self.count += 1
+
+    def setLocation(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def getLocation(self):
+        return self.x, self.y
+    
+    def getInfo(self):
+        return f"Piece [{self.count}]: Color: {self.color}, Location: ({self.x}, {self.y})"
