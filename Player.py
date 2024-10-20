eace8615cd59713ef04bea468e21a8d62ef03b56
Dureke Sanchez <dreamfox@berlin.cs.colostate.edu>
2024-10-20 10:11:57 -0600

    added player class

---
+++ Player.py
@@ -0,0 +1,16 @@
+import Piece
+
+class Player:
+
+    count = 0
+
+    def __init__(self, username):
+        self.username = username
+        self.ID = self.count
+        self.count += 1
+
+    def getUsername(self):
+        return self.username
+    
+    def getID(self):
+        return self.ID
